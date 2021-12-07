e0317e8098e0d845c8426bfdbc225dd71085c949
Daniel Etaat <danielbetaat@berkeley.edu>
2021-12-07 01:57:00 -0800

    fin. fr.

--- final/index.js
+++ final/index.js
@@ -58,13 +58,26 @@ let fs =
   '}';
 
 let mic;
+let micOn = false;
 let mandel;
 let spectrumOld;
 let slider;
+let songs = {};
 
 function setup() {
   let size = Math.min(windowWidth, windowHeight) / 1.4;
   createCanvas(size, size, WEBGL);
+
+  // Load Songs
+  songs["Last Christmas"] = loadSound('songs/LastChristmas.mp3');
+  songs["All I Want for Christmas Is You"] = loadSound('songs/All I Want for Christmas Is You.mp3');
+  songs["Snowman"] = loadSound('songs/Snowman.mp3');
+  songs["Piano Man"] = loadSound('songs/Piano Man.mp3');
+  songs["Four Seasons, Winter"] = loadSound('songs/Four Seasons, Winter.mp3');
+  songs["We Will Rock You"] = loadSound('songs/We Will Rock You.mp3');
+  songs["DNA"] = loadSound('songs/DNA.mp3');
+  songs["Moon"] = loadSound('songs/Moon.mp3');
+  songs["Time in a Bottle"] = loadSound('songs/Time in a Bottle.mp3');
 
   // Create an Audio input
   mic = new p5.AudioIn();
@@ -76,7 +89,6 @@ function setup() {
   shader(mandel);
   noStroke();
 
-  mic.start();
   spectrumOld = fft.analyze();
 
   // create a slider
@@ -95,9 +107,12 @@ function draw() {
     inp += Math.sqrt(Math.abs(spectrum[i] - spectrumOld[i]));
   }
   spectrumOld = spectrum;
-  console.log(inp);
 
-  inp = inp * mic.getLevel() / (spectrum.length);
+  if (micOn) {
+    inp = inp * mic.getLevel() / (spectrum.length);
+  } else {
+    inp = inp * 0.025 / (spectrum.length);
+  }
 
   if (inp < 0.001) {
       inp = 0.0;
@@ -109,11 +124,61 @@ function draw() {
   mandel.setUniform('r', acc * Math.PI + 2.5 + sin(millis() / 50000));
   mandel.setUniform('time', (millis() / 1000) % 100);
   mandel.setUniform('param', slider.value() / 10000);
-
-
   quad(-1, -1, 1, -1, 1, 1, -1, 1);
 }
 
 function mousePressed() {
-    userStartAudio();
+  userStartAudio();
+}
+
+function toggleMic(button) {
+  stopCurrentSong();
+  fft.setInput(mic);
+  if (micOn) {
+    mic.stop();
+    button.classList.remove("on");
+  } else {
+    mic.start();
+    button.classList.add("on");
+    document.getElementById("playb").classList.add("play");
+    document.getElementById("playb").classList.remove("pause");
+  }
+  micOn = !micOn;
+}
+
+let currentSong = null;
+function playSong(songName) {
+  stopCurrentSong();
+  currentSong = songName;
+  songs[songName].play();
+  fft.setInput(songs[songName]);
+  if (micOn) {
+    mic.stop();
+    document.getElementById("micb").classList.remove("on");
+  }
+}
+
+function stopCurrentSong() {
+  if (currentSong != null) {
+    songs[currentSong].stop();
+    currentSong = null;
+  }
+}
+
+function pauseCurrentSong() {
+  if (currentSong != null) {
+    songs[currentSong].stop();
+  }
+}
+
+function toggleSong(button) {
+  if (button.classList.contains("play")) {
+    playSong(document.getElementById("songs").value);
+    button.classList.remove("play");
+    button.classList.add("pause");
+  } else {
+    pauseCurrentSong();
+    button.classList.add("play");
+    button.classList.remove("pause");
+  }
 }
